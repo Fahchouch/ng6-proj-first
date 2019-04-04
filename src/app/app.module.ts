13f64c9f7fb8899e89dc4c6039ba54c113d3dee9
Fahchouch <fahchouch.hamza@gmail.com>
2019-04-04 15:02:52 +0100

    new test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component';
-import { UsersComponent } from './users/users.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
 import { HttpClientModule } from '@angular/common/http';
-import { DetailsComponent } from './details/details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
-    UsersComponent,
-    SidebarComponent,
-    DetailsComponent
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+/*export class DataUserService {
+
+  constructor() { }
+}*/
+
+export class DataUserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('https://reqres.in/api/users')
+  }
+}
